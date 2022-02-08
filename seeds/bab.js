1cfe96cb1de94e7a70fc9fce4524e968aa7d4b9a
nabilfatih <nabilakbarazzima@gmail.com>
2022-02-08 18:18:34 +0100

    add icons

--- seeds/bab.js
+++ seeds/bab.js
@@ -1,24 +1,56 @@
 module.exports.babMTK = [
-    // {
-    //     icon: ,
-    //     bab: ,
-    // },
-    "Fungsi",
-    "Matematika Dasar",
-    "Aljabar",
-    "Matriks",
-    "Eksponen",
-    "Teori Bilangan",
-    "Himpunan",
-    "Turunan",
-    "Aljabar Linear",
-    "Analisis"
+    {
+        icon: "/img/icon/fungsi-icon.png",
+        bab: "Fungsi"
+    },
+    {
+        icon: "/img/icon/mtkdasar-icon.png",
+        bab: "Matematika Dasar"
+    },
+    {
+        icon: "/img/icon/aljabar-icon.png",
+        bab: "Aljabar"
+    },
+    {
+        icon: "/img/icon/matriks-icon.png",
+        bab: "Matriks"
+    },
+    {
+        icon: "/img/icon/eksponen-icon.png",
+        bab: "Eksponen"
+    },
+    {
+        icon: "/img/icon/teoribilangan-icon.png",
+        bab: "Teori Bilangan"
+    },
+    {
+        icon: "/img/icon/himpunan-icon.png",
+        bab: "Himpunan"
+    },
+    {
+        icon: "/img/icon/turunan-icon.png",
+        bab: "Turunan"
+    },
+    {
+        icon: "/img/icon/aljabarlinear-icon.png",
+        bab: "Aljabar Linear"
+    },
+    {
+        icon: "/img/icon/analisis-icon.png",
+        bab: "Analisis"
+    }
 ]
 
 module.exports.babFisika = [
-    "Fisika Dasar",
+    {
+        icon: "/img/icon/fisikadasar-icon.png",
+        bab: "Fisika Dasar"
+    }
 ]
 
 module.exports.babInformatika = [
-    "Sistem Bilangan"
+    {
+        icon: "/img/icon/sistembilangan-icon.png",
+        bab: "Sistem Bilangan"
+    }
 ]
