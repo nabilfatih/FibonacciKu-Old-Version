abf0215c41545c65b4ad1e678d62a45fa86ea900
nabilfatih <nabilakbarazzima@gmail.com>
2022-02-11 23:09:35 +0100

    add seeds

---
+++ models/subbab.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const subbabSchema = new Schema ({
+    query: String,
+    querybab: String,
+    querysubbab: String,
+    pelajaran: String,
+    bab: String,
+    subbab: String,
+    judul: String,
+    link: String
+})
+
+module.exports = mongoose.model('SubBab', subbabSchema);
---
+++ seeds/dataSubBab.js
@@ -0,0 +1,341 @@
+module.exports.Fungsi = [
+    {
+        query: "matematika",
+        querybab: "fungsi",
+        querysubbab: "apa-itu-fungsi?",
+        pelajaran: "Matematika",
+        bab: "Fungsi",
+        subbab: "Apa itu Fungsi?"
+    },
+    {
+        query: "matematika",
+        querybab: "fungsi",
+        querysubbab: "menghitung-fungsi",
+        pelajaran: "Matematika",
+        bab: "Fungsi",
+        subbab: "Menghitung Fungsi"
+    },
+    {
+        query: "matematika",
+        querybab: "fungsi",
+        querysubbab: "menentukan-input-fungsi",
+        pelajaran: "Matematika",
+        bab: "Fungsi",
+        subbab: "Menentukan Input Fungsi"
+    },
+    {
+        query: "matematika",
+        querybab: "fungsi",
+        querysubbab: "range-domain",
+        pelajaran: "Matematika",
+        bab: "Fungsi",
+        subbab: "Range & Domain"
+    },
+    {
+        query: "matematika",
+        querybab: "fungsi",
+        querysubbab: "fungsi-komposisi",
+        pelajaran: "Matematika",
+        bab: "Fungsi",
+        subbab: "Fungsi Komposisi"
+    },
+    {
+        query: "matematika",
+        querybab: "fungsi",
+        querysubbab: "invers-fungsi",
+        pelajaran: "Matematika",
+        bab: "Fungsi",
+        subbab: "Invers Fungsi"
+    }
+]
+
+module.exports.MTKDasar = [
+    {
+        query: "matematika",
+        querybab: "mtk-dasar",
+        querysubbab: "penjumlahan-pengurangan",
+        pelajaran: "Matematika",
+        bab: "Matematika Dasar",
+        subbab: "Penjumlahan & Pengurangan"
+    },
+    {
+        query: "matematika",
+        querybab: "mtk-dasar",
+        querysubbab: "perkalian-pembagian",
+        pelajaran: "Matematika",
+        bab: "Matematika Dasar",
+        subbab: "Perkalian & Pembagian"
+    },
+    {
+        query: "matematika",
+        querybab: "mtk-dasar",
+        querysubbab: "urutan-operasi",
+        pelajaran: "Matematika",
+        bab: "Matematika Dasar",
+        subbab: "Urutan Operasi"
+    },
+    {
+        query: "matematika",
+        querybab: "mtk-dasar",
+        querysubbab: "apa-itu-bilangan-desimal?",
+        pelajaran: "Matematika",
+        bab: "Matematika Dasar",
+        subbab: "Apa itu Bilangan Desimal?"
+    },
+    {
+        query: "matematika",
+        querybab: "mtk-dasar",
+        querysubbab: "apa-itu-pecahan?",
+        pelajaran: "Matematika",
+        bab: "Matematika Dasar",
+        subbab: "Apa itu Pecahan?"
+    },
+    {
+        query: "matematika",
+        querybab: "mtk-dasar",
+        querysubbab: "rasio-perbandingan",
+        pelajaran: "Matematika",
+        bab: "Matematika Dasar",
+        subbab: "Rasio/Perbandingan"
+    },
+    {
+        query: "matematika",
+        querybab: "mtk-dasar",
+        querysubbab: "akar-kuadrat",
+        pelajaran: "Matematika",
+        bab: "Matematika Dasar",
+        subbab: "Akar Kuadrat"
+    }
+]
+
+module.exports.Aljabar = [
+    {
+        query: "matematika",
+        querybab: "aljabar",
+        querysubbab: "apa-itu-aljabar?",
+        pelajaran: "Matematika",
+        bab: "Aljabar",
+        subbab: "Apa itu Aljabar?"
+    },
+    {
+        query: "matematika",
+        querybab: "aljabar",
+        querysubbab: "menyederhanakan-aljabar",
+        pelajaran: "Matematika",
+        bab: "Aljabar",
+        subbab: "Menyederhanakan Aljabar"
+    },
+    {
+        query: "matematika",
+        querybab: "aljabar",
+        querysubbab: "mencari-nilai-pada-aljabar",
+        pelajaran: "Matematika",
+        bab: "Aljabar",
+        subbab: "Mencari Nilai pada Aljabar"
+    },
+    {
+        query: "matematika",
+        querybab: "aljabar",
+        querysubbab: "menfaktorkan-aljabar",
+        pelajaran: "Matematika",
+        bab: "Aljabar",
+        subbab: "Menfaktorkan Aljabar"
+    },
+    {
+        query: "matematika",
+        querybab: "aljabar",
+        querysubbab: "apa-itu-polinomial?",
+        pelajaran: "Matematika",
+        bab: "Aljabar",
+        subbab: "Apa itu Polinomial?"
+    }
+]
+
+module.exports.Matriks = [
+    {
+        query: "matematika",
+        querybab: "matriks",
+        querysubbab: "apa-itu-matriks?",
+        pelajaran: "Matematika",
+        bab: "Matriks",
+        subbab: "Apa itu Martiks?"
+    },
+    {
+        query: "matematika",
+        querybab: "matriks",
+        querysubbab: "perkalian-matriks",
+        pelajaran: "Matematika",
+        bab: "Matriks",
+        subbab: "Perkalian Matriks"
+    },
+    {
+        query: "matematika",
+        querybab: "matriks",
+        querysubbab: "jenis-jenis-matriks",
+        pelajaran: "Matematika",
+        bab: "Matriks",
+        subbab: "Jenis-Jenis Matriks"
+    },
+    {
+        query: "matematika",
+        querybab: "matriks",
+        querysubbab: "mencari-determinan-matriks",
+        pelajaran: "Matematika",
+        bab: "Matriks",
+        subbab: "Mencari Determinan Matriks"
+    },
+    {
+        query: "matematika",
+        querybab: "matriks",
+        querysubbab: "matriks-invers",
+        pelajaran: "Matematika",
+        bab: "Matriks",
+        subbab: "Matriks Invers"
+    },
+    {
+        query: "matematika",
+        querybab: "matriks",
+        querysubbab: "definit-pada-matriks",
+        pelajaran: "Matematika",
+        bab: "Matriks",
+        subbab: "Definit pada Matriks"
+    }
+]
+
+module.exports.Eksponen = [
+    {
+        query: "matematika",
+        querybab: "eksponen",
+        querysubbab: "sifat-eksponen",
+        pelajaran: "Matematika",
+        bab: "Eksponen",
+        subbab: "Sifat Eksponen"
+    },
+    {
+        query: "matematika",
+        querybab: "eksponen",
+        querysubbab: "contoh-soal-eksponen",
+        pelajaran: "Matematika",
+        bab: "Eksponen",
+        subbab: "Contoh Soal Eksponen"
+    },
+    {
+        query: "matematika",
+        querybab: "eksponen",
+        querysubbab: "apa-itu-logaritma?",
+        pelajaran: "Matematika",
+        bab: "Eksponen",
+        subbab: "Apa itu Logaritma?"
+    }
+]
+
+module.exports.TeoriBilangan = [
+    {
+        query: "matematika",
+        querybab: "teori-bilangan",
+        querysubbab: "bilangan-di-matematika",
+        pelajaran: "Matematika",
+        bab: "Teori Bilangan",
+        subbab: "Bilangan di Matematika"
+    },
+    {
+        query: "matematika",
+        querybab: "teori-bilangan",
+        querysubbab: "fakta-unik",
+        pelajaran: "Matematika",
+        bab: "Teori Bilangan",
+        subbab: "Fakta Unik"
+    },
+    {
+        query: "matematika",
+        querybab: "teori-bilangan",
+        querysubbab: "sifat-bilangan",
+        pelajaran: "Matematika",
+        bab: "Teori Bilangan",
+        subbab: "Sifat Bilangan"
+    }
+]
+
+module.exports.Himpunan = [
+    {
+        query: "matematika",
+        querybab: "himpunan",
+        querysubbab: "apa-itu-himpunan?",
+        pelajaran: "Matematika",
+        bab: "Himpunan",
+        subbab: "Apa itu Himpunan?"
+    },
+    {
+        query: "matematika",
+        querybab: "himpunan",
+        querysubbab: "jenis-jenis-himpunan",
+        pelajaran: "Matematika",
+        bab: "Himpunan",
+        subbab: "Jenis-Jenis Himpunan"
+    }
+]
+
+module.exports.Turunan = [
+    {
+        query: "matematika",
+        querybab: "turunan",
+        querybab: "apa-itu-turunan?",
+        pelajaran: "Matematika",
+        bab: "Turunan",
+        subbab: "Apa itu Turunan?"
+    },
+    {
+        query: "matematika",
+        querybab: "turunan",
+        querybab: "aturan-turunan",
+        pelajaran: "Matematika",
+        bab: "Turunan",
+        subbab: "Aturan Turunan"
+    },
+    {
+        query: "matematika",
+        querybab: "turunan",
+        querybab: "contoh-soal-turunan",
+        pelajaran: "Matematika",
+        bab: "Turunan",
+        subbab: "Contoh Soal Turunan"
+    }
+]
+
+module.exports.AljabarLinear = [
+    {
+        query: "matematika",
+        querybab: "aljabar-linear",
+        querysubbab: "persamaan-linear",
+        pelajaran: "Matematika",
+        bab: "Aljabar Linear",
+        subbab: "Persamaan Linear"
+    },
+    {
+        query: "matematika",
+        querybab: "aljabar-linear",
+        querysubbab: "dekomposisi",
+        pelajaran: "Matematika",
+        bab: "Aljabar Linear",
+        subbab: "Dekomposisi"
+    }
+]
+
+module.exports.Analisis = [
+    {
+        query: "matematika",
+        querybab: "analisis",
+        querysubbab: "limit-tak-hingga",
+        pelajaran: "Matematika",
+        bab: "Analisis",
+        subbab: "Limit Tak Hingga"
+    },
+    {
+        query: "matematika",
+        querybab: "analisis",
+        querysubbab: "limit-point",
+        pelajaran: "Matematika",
+        bab: "Analisis",
+        subbab: "Limit Point"
+    }
+]
