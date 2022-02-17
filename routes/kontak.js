56cfc97782ad262d6a25c79d0552e1677851ea2e
nabilfatih <nabilakbarazzima@gmail.com>
2022-02-17 13:38:03 +0100

    update routes

--- routes/kontak.js
+++ routes/kontak.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const kontak = require('../controllers/kontak');
 const catchAsync = require('../utils/catchAsync');
-const route = require('color-convert/route');
 
 router.get('/', catchAsync(kontak.kontak));
 
