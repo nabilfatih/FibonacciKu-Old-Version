53856ac8c0d373f504ad3fdd71e0801a00e66ef7
nabilfatih <nabilakbarazzima@gmail.com>
2022-03-04 19:05:00 +0100

    add error

--- controllers/profil.js
+++ controllers/profil.js
@@ -1,7 +1,11 @@
 const User = require('../models/user');
+const ExpressError = require('../utils/ExpressError');
 
-module.exports.profil = async (req, res) => {
+module.exports.profil = async (req, res, next) => {
     const username = await User.findOne({ username: req.params.username })
+    if(!username){
+        next(new ExpressError('Profil tidak ditemukan :(', 404))
+    }
     res.render('profil/profil', {
         username,
         user: req.user
