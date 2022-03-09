c23386ecfaf6b1690357c56b72bf6498f7c938ed
nabilfatih <nabilakbarazzima@gmail.com>
2022-03-10 00:00:33 +0100

    add tentang styles

--- public/js/tentang/sketch.js
+++ public/js/tentang/sketch.js
@@ -1,63 +1,44 @@
 const density = "        _.,-=+:;cba!?|}{0123456789$W#@Ñ";
-// const density = "𐌀	𐌁	𐌂	𐌃	𐌄	𐌅	𐌆	𐌇	𐌈	𐌉	𐌊	𐌋	𐌌	𐌍	𐌎	𐌏	𐌐	𐌑	𐌒	𐌓	𐌔	𐌕	𐌖	𐌗	𐌘	𐌙	𐌚";
-// const density = "Ñ@#W$9876543210?!abc;:+=-,._                ";
+let sourceText;
+let poem;
 let nabil;
-let video;
-let asciidiv;
-let keyed;
+let startIndex = 0;
+
+function preload() {
+  nabil = loadImage("/img/tentang/nabil.png");
+  sourceText = loadStrings("/img/tentang/nabil.txt");
+}
 
 function setup() {
-    noCanvas()
-    // var canvas = createCanvas(382, 475);
-    // canvas.parent('canva');
-    video = createCapture(VIDEO);
-    video.size(56, 56);
-    video.parent('canva')
-    asciidiv = createDiv();
-    asciidiv.parent('canva')
+  createCanvas(1385, 825).parent('matrix'); 
+  poem = sourceText.join(' ');
+  textFont("Courier-Bold");
 }
 
 function draw() {
-    // image(nabil, 0, 0, width, height);
-    let asciiImage = '';
-    video.loadPixels()
-    for(let j = 0; j < video.width; j++) {
-        for(let i = 0; i < video.height; i++) {
-            const pixelIndex = (i + j * video.width) * 4;
-            const r = video.pixels[pixelIndex + 0];
-            const g = video.pixels[pixelIndex + 1];
-            const b = video.pixels[pixelIndex + 2];
-            const avg = (r + g + b) / 3;
-            const len = density.length;
-            const charIndex = floor(map(avg, 0, 255, 0, len));
-
-            const c = density.charAt(charIndex);
-            if(c == ' ') asciiImage += '&nbsp;'
-            else asciiImage += c
-        }
-        asciiImage += '<br/>'
-    }
-    asciidiv.html(asciiImage);
-}
-
-//green screen
-function Key(image, color, level=50) {
-    image.loadPixels();
-    let out = new p5.Image(image.width, image.height)
-    out.loadPixels();
-    let keyVec = createVector(...color)
-    for (y = 0; y < image.height; y++) {
-      for (x = 0; x < image.width; x++) {
-        let col = image.get(x, y)
-        let colVec = createVector(...col)
-        let d = colVec.dist(keyVec)
-        let newCol = [...col]
-        if(d < level){
-          newCol[3] = 0
-        }
-        out.set(x, y, newCol)
+  background(0);
+  frameRate(10);
+  
+  let charIndex = startIndex;
+  let w = width / nabil.width;
+  let h = height / nabil.height;
+  nabil.loadPixels();
+  for (let j = 0; j < nabil.height; j++) {
+    for (let i = 0; i < nabil.width; i++) {
+        const pixelIndex = (i + j * nabil.width) * 4;
+        const r = nabil.pixels[pixelIndex + 0];
+        const g = nabil.pixels[pixelIndex + 1];
+        const b = nabil.pixels[pixelIndex + 2];
+        const avg = (r + g + b) / 3;
+        
+        noStroke();
+        fill(avg);      
+        textSize(w*1.2);
+        textAlign(CENTER, CENTER);
+        
+        text(poem.charAt(charIndex % poem.length), i * w + w * 0.5, j * h + h * 0.5);
+        charIndex++;
       }
-    }
-    out.updatePixels()
-    return out
   }
+  startIndex++;
+}
---
+++ public/js/tentang/time.js
@@ -0,0 +1,54 @@
+const s = ( sketch ) => {
+  
+    sketch.setup = () => {
+        sketch.createCanvas(400, 400);
+        sketch.angleMode(DEGREES);
+    };
+  
+    sketch.draw = () => {
+        sketch.background(33, 36, 61);
+        sketch.translate(200, 200);
+        sketch.rotate(-90); 
+
+        let hr = sketch.hour();
+        let mn = sketch.minute();
+        let sc = sketch.second();
+
+        sketch.strokeWeight(8);
+        sketch.stroke(255, 124, 124);
+        sketch.noFill();
+        let second = sketch.map(sc, 0, 60, 0, 360);
+        sketch.arc(0, 0, 385, 385, 0, second);
+
+        sketch.push();
+        sketch.rotate(second)
+        sketch.stroke(255, 124, 124);
+        sketch.line(0, 0, 100, 0)
+        sketch.pop()
+
+        sketch.stroke(255, 208, 130);
+        let minute = sketch.map(mn, 0, 60, 0, 360);
+        sketch.arc(0, 0, 365, 365, 0, minute);
+
+        sketch.push();
+        sketch.rotate(minute)
+        sketch.stroke(255, 208, 130);
+        sketch.line(0, 0, 75, 0)
+        sketch.pop()
+
+        sketch.stroke(136, 225, 242);
+        let hour = sketch.map(hr % 12, 0, 12, 0, 360)
+        sketch.arc(0, 0, 345, 345, 0, hour);
+
+        sketch.push();
+        sketch.rotate(hour)
+        sketch.stroke(136, 225, 242);
+        sketch.line(0, 0, 50, 0)
+        sketch.pop()
+
+        sketch.stroke(255);
+        sketch.point(0, 0)
+    };
+  };
+  
+let myp5 = new p5(s, 'waktu');
