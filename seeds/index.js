bb7c460b871ba4a02812878464800a81ec74ad3b
nabilfatih <nabilakbarazzima@gmail.com>
2022-04-12 22:16:30 +0200

    update

--- seeds/index.js
+++ seeds/index.js
@@ -8,8 +8,6 @@ const { DataBab } = require('./dataBab');
 const { DataSubBab } = require('./dataSubBab');
 const { DataKonten } = require('./dataKonten');
 
-// const dbUrl = "mongodb+srv://nabilfatih:Nabil16112001@development.xt5rc.mongodb.net/Development?retryWrites=true&w=majority";
-const dbUrl = 'mongodb+srv://fibonacciku:Fatih16112001@fibonacciku.hjx4b.mongodb.net/FibonacciKu?retryWrites=true&w=majority';
 const dbUrlProduction = 'mongodb://localhost:27017/fibonacciku';
 
 mongoose.connect(dbUrl, {
